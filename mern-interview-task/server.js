a77dafb29c21ede40eb699f58078d6f106939bca
Praveen Chasta <praveen.chastaa@gmail.com>
2023-11-26 08:26:34 +0000

    Hexadecimal backend

--- mern-interview-task/server.js
+++ mern-interview-task/server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 const PORT = 3000;
 
-app.get("/v1/users", async (req, res) => {
+app.get("/v1/users", async (request, response) => {
   try {
     const users = await axios.get("https://jsonplaceholder.typicode.com/users");
     const posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
@@ -13,10 +13,10 @@ app.get("/v1/users", async (req, res) => {
       posts: posts.data.filter((post) => post.userId === user.id),
     }));
 
-    res.json(combinedData);
+    response.json(combinedData);
   } catch (error) {
     console.error("Error fetching data:", error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    response.status(500).json({ error: "Internal Server Error" });
   }
 });
 
